5af91393899ce8f5de366988941c19df686a36ff
ashish <ashishsingh159258@gmail.com>
2024-05-21 18:58:46 +0530

    eureka add

---
+++ src/main/java/com/techhitter/app/config/ChatConfiguration.java
@@ -0,0 +1,9 @@
+package com.techhitter.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatConfiguration {
+
+}
